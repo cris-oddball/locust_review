9c0acc0476651d4007649206219b82e5c8a640d0
cris-oddball <cris.stoddard@oddball.io>
2024-08-09 07:04:50 -0600

    finish email test

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ A look into using Locust to validate notifications. This is only for demo purpos
 4. Run `poetry install`
 5. Copy the `example.env` file locally to .env and add information (project would use boto3 to pull these secrets)
 6. Run the tests:
+```
+locust -f load_test_email.py -u 1 -r 1 --run-time 2s --host https://sandbox-api.va.gov/vanotify --headless
+```
--- load_test_email.py
+++ load_test_email.py
@@ -15,8 +15,39 @@ class SendEmail(HttpUser):
     """
     Sends requests to POST v2/notifications/email
     """
-
     def on_start(self) -> None:
-        self.api_key = os.environ.get("API_KEY")
-        self.email_template_id = os.environ.get("EMAIL_TEMPLATE_ID")
-        self.service_id = os.environ.get("SERVICE_ID")
+        self.api_key = os.environ.get('API_KEY')
+        self.email_template_id = os.environ.get('EMAIL_TEMPLATE_ID')
+        self.service_id = os.environ.get('SERVICE_ID')
+
+    def _get_jwt(self) -> str:
+        header = {'typ': 'JWT', 'alg': 'HS256'}
+        combo = {}
+        currentTimestamp = int(time.time())
+        data = {
+            'iss': self.service_id,
+            'iat': currentTimestamp,
+            'exp': currentTimestamp + 30,
+            'jti': 'jwt_nonce'
+        }
+        combo.update(data)
+        combo.update(header)
+        encoded_jwt = jwt.encode(combo, self.api_key, algorithm='HS256')
+        return encoded_jwt
+
+    @task
+    def send(self):
+        encoded_jwt = self._get_jwt()
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f"Bearer {encoded_jwt}"
+        }
+        payload = {
+            'template_id': self.email_template_id,
+            'email_address': 'vanotify.qa@gmail.com'
+        }
+        self.client.post(
+            '/v2/notifications/email',
+            json=payload,
+            headers=headers
+        )
